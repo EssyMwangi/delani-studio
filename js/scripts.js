4c243e10fcf4fb6ceebd56021da5e52d70ee5a43
Essy Mwangi <sonnieessy@gmail.com>
2020-02-21 21:04:45 +0300

    added css and jquery to improve on functionality

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $(".design-icon").click(function(){
+    $("#design-image").toggle();
+    $('#design').toggle();
+  });
+  $(".development-icon").click(function(){
+    $("#development-image").toggle();
+    $('#development').toggle();
+  });
+  $(".product-management-icon").click(function(){
+    $("#product-image").toggle();
+    $('#product-management').toggle();
+  });
+
+  $("div.image-frame").hover(function(){
+    
+
+    $(".overlay1").show().addClass("mask flex-center rgba-red-strong");
+    }); 
+
+  $("#btn").click(function(){
+
+    var name= $("#name").val();
+    var email=$("#email").val();
+    var message=$("#comment").val();
+
+    alert("Thank you " + name + " for your feedback.You will receive an email shortly via " + email);
+
+  });
+
+});
+
+
