204a6b2fa6e1d01f5eee7e1b64dc8b9d33a1e89b
Essy Mwangi <sonnieessy@gmail.com>
2020-02-21 21:48:07 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,29 @@
+ # DELANI STUDIO
+
+---
+ 
+ ## DESCRIPTION
+This weeks task is to implement everything I have learnt from Bootstrap and jQuery to recreate a landing page for a fictional studio.
+
+---
+
+## TECHNOLOGIES USED 
+* HTML
+* CSS
+* JAVASCRIPT
+* JQUERY
+* MDB
+* BOOTSTRAP
+
+---
+### PROJECT SETUP INSTRUCTIONS
+The fictional studio called Delani Studio is run via a browser through this live link: <>
+
+---
+
+### PROJECT AUTHOR
+
+- Essy M Mwangi <sonnieessy@gmail.com>
+
+
+---
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
   //what we do//
-  $(".design-icon").click(function(){
+  $(".design-icon").click(function () {
     $("#design-image").toggle();
     $('#design').toggle();
   });
-  $(".development-icon").click(function(){
+  $(".development-icon").click(function () {
     $("#development-image").toggle();
     $('#development').toggle();
   });
-  $(".product-management-icon").click(function(){
+  $(".product-management-icon").click(function () {
     $("#product-image").toggle();
     $('#product-management').toggle();
   });
   //portfolio//
-  $("div.image-frame").hover(function(){
+  $("div.image-frame").hover(function () {
     $(".overlay1").show().addClass("mask flex-center rgba-red-strong");
-    }); 
+  });
   //contact us//
-  $("#btn").click(function(){
-    var name= $("#name").val();
-    var email=$("#email").val();
-    var message=$("#comment").val();
+  $("#btn").click(function () {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#comment").val();
     alert("Thank you " + name + " for your feedback.You will receive an email shortly via " + email);
   });
 });
